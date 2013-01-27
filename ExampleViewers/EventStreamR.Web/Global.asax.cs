ca7cd053f1816204e06539076bbe61c083373f8e
Ryan Tomlinson <tomlinsonryan@gmail.com>
2013-01-27 14:38:54 +0000

    Integrated twitter.bootstrap.mvc4 for a nice way to add navigation in bootstrap

--- ExampleViewers/EventStreamR.Web/Global.asax.cs
+++ ExampleViewers/EventStreamR.Web/Global.asax.cs
@@ -21,6 +21,8 @@ namespace EventStreamR.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BootstrapSupport.BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
+            BootstrapMvcSample.ExampleLayoutsRouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
