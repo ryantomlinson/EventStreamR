4418ccbab474ddc26c8008fa1ecc17abba7f5b0f
steve higgs <stevejhiggs@gmail.com>
2013-01-27 11:23:16 +0000

    strip down the example client site to the minimum packages required and fix reference hints

--- ExampleClients/EventStreamR.Web.Client/Global.asax.cs
+++ ExampleClients/EventStreamR.Web.Client/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace EventStreamR.Web.Client
@@ -21,7 +20,6 @@ namespace EventStreamR.Web.Client
 			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
 		}
 	}
 }
